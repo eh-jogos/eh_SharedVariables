2a9f58c5311f545657f27317bdf71e5e2ae58e56
Daniel Queiroz Porto <danielvandalk@gmail.com>
2021-03-27 15:42:45 -0300

    Update addon folder with new stuff in submodule branch
    Squashed commit of the following:
    
    commit 1749fbeed53bfff72e36ef3284d4ee3500cfe9ad
    Merge: 2d81701 91cb5f1
    Author: Daniel Queiroz Porto <danielvandalk@gmail.com>
    Date:   Sat Mar 27 15:02:46 2021 -0300
    
        Merge remote-tracking branch 'origin/project/dcors' into submodule
    
    commit 91cb5f13cbb1569e038b03af9616fb86ce9e272c
    Author: Daniel Queiroz Porto <danielvandalk@gmail.com>
    Date:   Sat Mar 27 09:28:07 2021 -0300
    
        Make NodePath variables session only by default
    
        It might also make sense to not show any values in the inspector,
        maybe add some warning or instruction on how to properly use it?
    
    commit b4e166e1d1226239e96c33d74551ea38c0e70fb4
    Author: Daniel Queiroz Porto <danielvandalk@gmail.com>
    Date:   Sat Mar 27 07:39:54 2021 -0300
    
        Adds Vector3Variable
    
    commit 2d81701c76b4e5201f291162564f189344af412b
    Author: Daniel Queiroz Porto <danielvandalk@gmail.com>
    Date:   Sun Mar 21 09:41:52 2021 -0300
    
        move everything to root to make a "submodule" branch

--- addons/eh_jogos.shared_variables/variables/Vector3Variable.gd
+++ addons/eh_jogos.shared_variables/variables/Vector3Variable.gd
@@ -1,9 +1,9 @@
-# NodePath that can be saved in disk like a custom resource.
+# Vector3 that can be saved in disk like a custom resource. 
 # Used as [Shared Variables] so that the data it holds can be accessed and modified from multiple 
 # parts of the code. Based on the idea of Unity's Scriptable Objects and Ryan Hipple's Unite Talk.
 # @category: Shared Variables
 tool
-class_name NodePathVariable
+class_name Vector3Variable
 extends SharedVariable
 
 ### Member Variables and Dependencies -------------------------------------------------------------
@@ -16,10 +16,10 @@ extends SharedVariable
 #--- public variables - order: export > normal var > onready --------------------------------------
 
 # Shared Variable value
-var value: NodePath = NodePath("") setget _set_value, _get_value
+var value: Vector3 = Vector3.ZERO setget _set_value, _get_value
 
 # Defautl value in case you're using `is_session_only`
-var default_value: NodePath = NodePath("") setget _set_default_value, _get_default_value
+var default_value: Vector3 = Vector3.ZERO setget _set_default_value, _get_default_value
 
 #--- private variables - order: export > normal var > onready -------------------------------------
 
@@ -28,9 +28,10 @@ var default_value: NodePath = NodePath("") setget _set_default_value, _get_defau
 
 ### Built in Engine Methods -----------------------------------------------------------------------
 
-func _init() -> void:
-	is_session_only = true
+### -----------------------------------------------------------------------------------------------
 
+
+### Public Methods --------------------------------------------------------------------------------
 
 func _get_property_list() -> Array:
 	var properties: = []
@@ -49,16 +50,11 @@ func _get_property_list() -> Array:
 
 
 func is_class(p_class: String) -> bool:
-	return p_class == "NodePathVariable" or .is_class(p_class)
+	return p_class == "DesiredTypeVariable" or .is_class(p_class)
 
 
 func get_class() -> String:
-	return "NodePathVariable"
-
-### -----------------------------------------------------------------------------------------------
-
-
-### Public Methods --------------------------------------------------------------------------------
+	return "DesiredTypeVariable"
 
 ### -----------------------------------------------------------------------------------------------
 
@@ -68,14 +64,16 @@ func get_class() -> String:
 func _get_value_property_dict(property_name: String) -> Dictionary:
 	var dict = {
 		name = "%s"%[property_name],
-		type = TYPE_NODE_PATH, 
+		# Pay attention to this though, it has to be UPPERCASE, and if it's a custom type, 
+		# probably TYPE_OBJECT
+		type = TYPE_VECTOR3, 
 		usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
 		hint = PROPERTY_HINT_NONE,
 	}
 	return dict
 
 
-func _set_value(p_value: NodePath) -> void:
+func _set_value(p_value: Vector3) -> void:
 	if _should_reset_value():
 		p_value = get_meta("default_value")
 	set_meta("value", p_value)
@@ -84,33 +82,28 @@ func _set_value(p_value: NodePath) -> void:
 	_auto_save()
 
 
-func _get_value() -> NodePath:
-	var meta_value = NodePath("")
+func _get_value() -> Vector3:
+	var meta_value = Vector3.ZERO
 	if _should_reset_value():
 		meta_value = get_meta("default_value")
 		_set_value(meta_value)
-	elif value == NodePath("") and has_meta("value"):
+	elif value == Vector3.ZERO and has_meta("value"):
 		meta_value = get_meta("value")
 	else:
 		meta_value = value
-	
-	if meta_value == NodePath("") and resource_name != "":
-		push_error("Undefined nodepath in %s"%[resource_name])
-		assert(false)
-	
 	return meta_value
 
 
-func _set_default_value(p_value: NodePath) -> void:
+func _set_default_value(p_value: Vector3) -> void:
 	set_meta("default_value", p_value)
 	_set_value(p_value)
 	default_value = p_value
 	_auto_save(true)
 
 
-func _get_default_value() -> NodePath:
-	var meta_value = NodePath("")
-	if default_value == NodePath("") and has_meta("default_value"):
+func _get_default_value() -> Vector3:
+	var meta_value = Vector3.ZERO
+	if default_value == Vector3.ZERO and has_meta("default_value"):
 		meta_value = get_meta("default_value")
 	else:
 		meta_value = default_value
